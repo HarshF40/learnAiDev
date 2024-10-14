392a916c87953121912231a00f702a85eb55a194
HarshF40 <harshgaonkar57@gmail.com>
2024-10-14 23:01:18 +0530

    learn python

---
+++ python/compare.py
@@ -0,0 +1,17 @@
+x = int(input("Enter X: "))
+y = int(input("Enter Y: "))
+
+if x > y:
+    print("X is greater than Y")
+if y > x:
+    print("Y is greater than X")
+if x == y:
+    print("X is equal to Y")
+
+    ### elif ###
+    if x>y:
+        print("X is greater than y")
+    elif x<y:
+        print("X id less than y")
+    elif  x==y:
+        print("X is equal to Y")
