a5a43f543c947eba61ad4970de1fb58fb7b6b2ed
HarshF40 <harshgaonkar57@gmail.com>
2024-10-15 22:19:56 +0530

    learn dict in python

--- python/dict.py
+++ python/dict.py
@@ -13,5 +13,8 @@ print(fruit["pink"])
 fruit2 = [
         {"color" : "red","fruit" : "apple"},
         {"color" : "yellow","fruit":"banana"},
-        {""},
+        {"color" : "pink","fruit":"guava"}
         ]
+
+for fruit2 in fruit2:
+    print(fruit2["color"],fruit2["fruit"])
