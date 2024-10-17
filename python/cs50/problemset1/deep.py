40766babc1c4a1f85c2cd71d640cdde3a79cba2d
HarshF40 <harshgaonkar57@gmail.com>
2024-10-17 08:25:23 +0530

    Start problem set 1

---
+++ python/cs50/problemset1/deep.py
@@ -0,0 +1,5 @@
+ans = input("What is the Answer to the Great question of the life, the universe, and everything?\n")
+if(ans == "42" or ans == "forty-two" or ans == "forty two"):
+    print("Yes")
+else:
+    print("No")
