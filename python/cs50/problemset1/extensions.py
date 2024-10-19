c5f535aa5ea1b363b9722ed232bf2a228dab45cb
HarshF40 <harshgaonkar57@gmail.com>
2024-10-19 23:39:11 +0530

    simplify extensions.py

--- python/cs50/problemset1/extensions.py
+++ python/cs50/problemset1/extensions.py
@@ -1,21 +1,17 @@
 file = input("File Name: ")
 str_len = len(file)
 
-i = 0
-flag = True
+dot_pos = file.rfind(".") #this will return the position of the dot in the text file
 
-while file[i]!="." and i<str_len-1:
-        i+=1
+file_ext = file[dot_pos+1:] #this will return the string from the dot pos to the end of the string
 
-if flag:
-    file_ext = file[i+1:str_len] #this will the other part... i+1-str_len chracters from i+1
-    if file_ext == "jpeg" or file_ext == "jpg" or file_ext == "gif" or file_ext == "png":
-        print(f"image/{file_ext}")
-    elif file_ext == "pdf":
-        print(f"document/{file_ext}")
-    elif file_ext == "txt":
-        print(f"text/{file_ext}")
-    elif file_ext == "zip":
-        print(f"compressed_file/{file_ext}")
-    else:
-         print(f"application/octet-stream")
+if file_ext in {"jpg","jpeg","png","gif"}:
+    print(f"image/{file_ext}")
+elif file_ext == "pdf":
+    print(f"document/{file_ext}")
+elif file_ext == "txt":
+    print(f"text/{file_ext}")
+elif file_ext == "zip":
+    print(f"compressed_file/{file_ext}")
+else:
+    print(f"application/octet-stream")
