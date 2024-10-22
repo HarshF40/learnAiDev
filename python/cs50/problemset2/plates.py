ab80769e6496ea59d38e280426b7b0e6009bc6bc
HarshF40 <harshgaonkar57@gmail.com>
2024-10-22 08:38:09 +0530

    plates.py

---
+++ python/cs50/problemset2/plates.py
@@ -0,0 +1,16 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    if (len(s) > 6) or len(s)<2 or not (s[0].isalpha() and s[1].isalpha()):
+        return False
+    for i in range (len(s)-1):
+        if s[i].isdigit() and s[i+1].isalpha() or not s[i].isalnum() or (s[i].isalpha() and s[i+1] == "0"):
+            return False
+    return True        
+    
+main()
