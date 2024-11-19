d25f3769ecc8b5ff27df42ebe2f3dbd7ac304461
HarshF40 <harshgaonkar57@gmail.com>
2024-11-19 08:17:39 +0530

    regex

--- python/regexs/characters_intro.py
+++ python/regexs/characters_intro.py
@@ -5,6 +5,16 @@
 # ? --> 0 or 1 repitations
 # {m} --> m repitations
 # {m,n} --> m-n repitations
+# ^ --> match the start of the string
+#$ --> match the end of the string
+#[] --> set of characters,takes care of .
+#[^] --> complementing the set,takes care of .
+#\d --> any decimal digit
+#\D --> not a Decimal digit
+#\s --> whitespace characters
+#\S --> not a white space characters
+#\w --> word (can also contain number and underscore)
+#\w --> not a word
 
 #checking for valid email
 
@@ -12,7 +22,7 @@ import re
 
 email = input("Enter Email: ").strip()
 
-if re.search(r".+@.+\..+",email) :
+if re.search(r"^\w@\w\.(com|edu|net|gov$",email,re.IGNORECASE) :
   print("Valid")
 else :
   print("Invalid")
