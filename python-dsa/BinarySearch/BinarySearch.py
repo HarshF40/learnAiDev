35f0e6a4738bb1f20cf3c7316e0802515f1e1117
HarshF40 <harshgaonkar57@gmail.com>
2024-12-21 00:24:53 +0530

    Binary Search first occurance check

--- python-dsa/BinarySearch/BinarySearch.py
+++ python-dsa/BinarySearch/BinarySearch.py
@@ -4,15 +4,21 @@ def BinarySearch(arr, val):
     while low != high :
         position = int((low + high) / 2)
         if arr[position] == val :
+            position = get_first_occurance(arr, val, position)
             return position
         elif arr[position] > val :
             low = position + 1
         elif arr[position] < val :
             high = position - 1
 
+def get_first_occurance(arr, val, pos):
+    while arr[pos] == val :
+        pos -= 1
+    return pos + 1
+
 def main():
-    arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    val = 10
+    arr = [10, 10, 10, 10, 10, 9, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    val = 9
     print(BinarySearch(arr, val))
 
 if __name__ == "__main__":
