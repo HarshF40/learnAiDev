834faed5d2a6ea3fdbea45dc8de2ac4eb7acfe1c
HarshF40 <harshgaonkar57@gmail.com>
2024-12-23 23:37:32 +0530

    Binary Rotated List Solved

--- python-dsa/BinarySearch/BinaryRotatedList.py
+++ python-dsa/BinarySearch/BinaryRotatedList.py
@@ -1,33 +1,21 @@
 def rotations(arr):
     low = 0
     high = len(arr) - 1
-    while low != high :
-        position = int((low+high)/2)
-        print(f"Position: {position}")
+    while low<=high :
+        mid = (low+high)//2
+        print(mid, low, high)
 
-        if high < len(arr) - 1 and low > -1 :
-            if arr[high - 1] > arr[high] < arr[high + 1]:
-                return high
-            elif arr[low - 1] > arr[low] < arr[low + 1]:
-                return low
+        if mid > 0 and (arr[mid - 1] > arr[mid] or low == high == mid) :
+            return mid
+        elif (abs(arr[low] - arr[mid]) < abs(arr[high] - arr[mid])) and (arr[low - 1] < arr[low]) :
+            low = mid + 1
+        else :
+            high = mid - 1
 
-        if arr[position - 1] < arr[position] < arr[position + 1]:
-            print("Case 1")
-            high = position - 1
-
-        elif arr[position - 1] > arr[position] > arr[position + 1]:
-            print("Case 2")
-            low = position + 1
-
-        elif arr[position - 1] < arr[position] > arr[position + 1]:
-            print("Case 3")
-            return position + 1
-
-        elif arr[position - 1] > arr[position] < arr[position + 1]:
-            return position
+    return 0
 
 def main():
-    arr = [ 19, 25, 29, 3, 5, 6, 7, 9, 11, 14 ]
+    arr = [ 6,7,8,9,1,2,3,4,5 ]
     print(f"Roatations: {rotations(arr)}")
 
 if __name__ == "__main__":
