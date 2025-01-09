11d3182e97161c7de8ccdd05b37840e9a284bfe0
harsh <harshgaonkar57@gmail.com>
2025-01-09 13:48:01 +0530

    Improve delete bst node

--- python-dsa/BinarySearchTree/deleteBST.py
+++ python-dsa/BinarySearchTree/deleteBST.py
@@ -48,87 +48,116 @@ class TreeNode: #BST node defination
         print(space*level+ f"{self.key}")
         self.left.display(space, level+1) if self.left else None
 
+#def delete(node, key, parent = None):
+#    if node is None:
+#        print("No Element to delete")
+#        return
+#    if key > node.key :
+#        node.right = delete(node.right, key, node)
+#    elif key < node.key :
+#        node.left = delete(node.left, key, node)
+#
+#    if key == node.key:
+#        if parent is None:
+#            if node.right:
+#                node.right.parent = parent
+#                next = node.right
+#                while next.left is not None:
+#                    next = next.left
+#                if node.left :
+#                    node.left.parent = next
+#                    next.left = node.left
+#                    node.left = None
+#                else :
+#                    next.left = None
+#                node = node.right
+#                return node
+#
+#            if node.left :
+#                node.left.parent = parent
+#                node = node.left
+#                return node
+#                    
+#
+#        if node.right:
+#            if node.key < parent.key if parent else None:
+#                parent.left = node.right
+#                node.right.parent = parent
+#                node.right.left = node.left
+#                node = node.right
+#                return node
+#            if node.key > parent.key if parent else None :
+#                parent.right = node.right
+#                node.right.parent = parent
+#                node.right.left = node.left
+#                node = node.right
+#                return node
+#
+#        elif node.left:
+#            if node.key < parent.key if parent else None :
+#                parent.left = node.left
+#                node.left.parent = parent
+#                node = node.left
+#                return node
+#            if node.key > parent.key if parent else None :
+#                parent.right = node.left
+#                node.left.parent = parent
+#                node = node.left
+#                return node
+#
+#        elif node.left is None and  node.right is None:
+#            if node.key < parent.key if parent else None :
+#                node = None
+#                parent.left = node
+#                return node
+#            elif node.key > parent.key if parent else None :
+#                node = None
+#                parent.right = node
+#                return node
+#    return node
+
 def delete(node, key, parent = None):
     if node is None:
-        print("No Element to delete")
-        return
-    if key > node.key :
-        node.right = delete(node.right, key, node)
+        print("No element found to delete")
+        return None
+
     elif key < node.key :
         node.left = delete(node.left, key, node)
-
-    if key == node.key:
-        if parent is None:
-            if node.right:
-                node.right.parent = parent
-                next = node.right
-                while next.left is not None:
-                    next = next.left
-                if node.left :
-                    node.left.parent = next
-                    next.left = node.left
-                    node.left = None
-                else :
-                    next.left = None
-                node = node.right
-                return node
-
-            if node.left :
-                node.left.parent = parent
-                node = node.left
-                return node
-                    
-
-        if node.right:
-            if node.key < parent.key if parent else None:
-                parent.left = node.right
-                node.right.parent = parent
-                node.right.left = node.left
-                node = node.right
-                return node
-            if node.key > parent.key if parent else None :
-                parent.right = node.right
-                node.right.parent = parent
-                node.right.left = node.left
-                node = node.right
-                return node
-
-        elif node.left:
-            if node.key < parent.key if parent else None :
-                parent.left = node.left
-                node.left.parent = parent
-                node = node.left
-                return node
-            if node.key > parent.key if parent else None :
-                parent.right = node.left
-                node.left.parent = parent
-                node = node.left
-                return node
-
-        elif node.left is None and  node.right is None:
-            if node.key < parent.key if parent else None :
-                node = None
-                parent.left = node
-                return node
-            elif node.key > parent.key if parent else None :
-                node = None
-                parent.right = node
-                return node
+    elif key > node.key :
+        node.right = delete(node.right, key, parent)
+    else :
+        if node.left is None and node.right is None :
+            return None
+        elif node.left is None :
+            node.right.parent = parent
+            return node.right
+        elif node.right is None :
+            node.left.parent = parent
+            return node.left
+        else :
+            next = node.right
+            while next.left is not None :
+                next = next.left
+            next.left = node.left
+            node.left.parent = next
+            return node.right
     return node
 
 def main():
     Tree = TreeNode()
-    user0 = node(-1)
-    user1 = node(-2)
-    user2 = node(-3)
-    user3 = node(-4)
-    user4 = node(-5)
-    user5 = node(-6)
-    user6 = node(-7)
-    user7 = node(-8)
-    user8 = node(-9)
-    user9 = node(-10)
-    user10 = node(-11)
+    user0 = node(9)
+    user1 = node(6)
+    user2 = node(4)
+    user3 = node(7)
+    user4 = node(3)
+    user5 = node(5)
+    user6 = node(15)
+    user7 = node(11)
+    user8 = node(16)
+    user9 = node(10)
+    user10 = node(13)
+    user11 = node(12)
+    user12 = node(14)
     Tree.insert(user0)
     Tree.insert(user1)
     Tree.insert(user2)
@@ -138,11 +167,13 @@ def main():
     Tree.insert(user6)
     Tree.insert(user7)
     Tree.insert(user8)
-    Tree.insert(user9)
+    #Tree.insert(user9)
     Tree.insert(user10)
+    Tree.insert(user11)
+    #Tree.insert(user12)
     Tree.display()
     print("----------------------------------------------------------------------------------")
-    Tree = delete(Tree, -1)
+    Tree = delete(Tree, 9)
     Tree.display() if Tree else None
 
 if __name__ == "__main__":
